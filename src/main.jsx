11fa548e47501137b88513431b1ee39e47faba8c
LaraRobertson <lara@lararobertson.com>
2026-01-20 11:00:36 -0500

    Fix API key and GraphQL endpoint configuration

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import { Authenticator } from "@aws-amplify/ui-react";
 to your backend configuration using the Amplify CLI, the configuration in
 amplifyconfiguration.json will update automatically. */
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './aws-exports.js';
+import amplifyconfig from './amplifyconfiguration.json';
 
 
 const isLocalhost = Boolean(
